f408ea18d190644524fb0a6fc0add410f571e322
khoanx1007 <90041062+khoanx1007@users.noreply.github.com>
2021-10-22 17:47:18 +0700

    Add files via upload

---
+++ B19/bai19.js
@@ -0,0 +1,106 @@
+$(document).ready(function(){
+	$("#bars").click(function(){
+		$(this).toggleClass("fa-times");
+		$(".navi").toggleClass("active");
+	});
+    var plus = '<i class="fa fa-plus"></i>';
+	var minus = '<i class="fa fa-minus"></i>'
+    $('#p1').show();
+    $("#b1").click(function(){
+        if($('#b1').html() == plus) {
+            $('#b1').html(minus);
+        }else {
+            $('#b1').html(plus);
+        }
+
+        $('#b2').html(plus);
+        $('#b3').html(plus);
+        $('#b4').html(plus);
+
+        $('#p1').slideToggle();
+        $('#p2').hide();
+        $('#p3').hide();
+        $('#p4').hide();
+    })
+    $('#p2').hide();
+	$('#b2').click(function() {
+	if($('#b2').html() == plus) {
+    	$('#b2').html(minus);
+	}else {
+    	$('#b2').html(plus);
+	}
+
+    	$('#b1').html(plus);
+		$('#b4').html(plus);
+		$('#b3').html(plus);
+
+		$('#p2').slideToggle();
+		$('#p1').hide();
+		$('#p3').hide();
+		$('#p4').hide();
+	});
+
+	$('#p3').hide();
+	$('#b3').click(function() {
+	    if($('#b3').html() == plus) {
+			$('#b3').html(minus);
+		}else {
+			$('#b3').html(plus);
+			}
+
+	    	$('#b1').html(plus);
+			$('#b2').html(plus);
+			$('#b4').html(plus);
+
+			$('#p3').slideToggle();
+			$('#p4').hide();
+			$('#p2').hide();
+			$('#p1').hide();
+	});
+
+	$('#p4').hide();
+	$('#b4').click(function() {
+		if($('#b4').html() == plus) {
+			$('#b4').html(minus);
+		}else {
+			$('#b4').html(plus);
+		}
+
+			$('#b1').html(plus);
+			$('#b2').html(plus);
+			$('#b3').html(plus);
+
+			$('#p4').slideToggle();
+			$('#p3').hide();
+		    $('#p2').hide();
+			$('#p1').hide();
+	});
+	$('.slide').bxSlider();
+		var $form = $("form");
+		$successMsg = $(".alert");
+		jQuery.validator.addMethod("customEmail", function(value, element) { 
+			return this.optional( element ) || /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test( value ); 
+		  }, "Please enter valid email address!");
+  		if($form.length){
+			$form.validate({
+				rules: {
+					  email: {
+						required: true,
+						customEmail: true
+						  }
+					},
+				messages: {
+					required: 'Please enter email!',
+					  email: "Please specify a valid email address"
+						},
+				errorPlacement: function(error, element) 
+				{
+					error.insertBefore(element);
+				},
+				submitHandler: function() {
+					$successMsg.show();
+				  }
+			});
+		  }
+  	
+})
